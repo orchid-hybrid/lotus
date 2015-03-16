1baa76353cebeaf1e926933713ad7f66ad18b325
yuuko <yuuko@users.noreply.github.com>
2015-03-16 19:45:01 -0400

    finished transform from src-exts AST to our AST

--- Lotus.hs
+++ Lotus.hs
@@ -29,17 +29,17 @@ data Type = A Atomic | Type :-> Type
 data Atomic = BoolTy | IntTy | ListTy Atomic
 
 transform :: Module -> Maybe [Term]
-transform (Module _ _ _ _ _ _ ds) = foldM (\a d -> do
+transform (Module _ _ _ _ _ _ ds) = (flip3 foldM) ds [] $ \a d -> do
     t <- transformDeclaration d
-    return (t:a)) [] ds
+    return (t:a)
 
 transformDeclaration :: Decl -> Maybe Term
 transformDeclaration (FunBind [(Match _ (Ident i) vs _ (UnGuardedRhs b) _)]) = do
     b' <- transformExpression b
 
-    (v':vs') <- foldM (\a v -> case v of
+    (v':vs') <- (flip3 foldM) vs [] $ \a v -> case v of
         (PVar (Ident i)) -> return (i:a)
-        _ -> Nothing) [] vs
+        _ -> Nothing
 
     return (Function i (foldr (\v a -> Lambda v a) (Lambda v' b') (reverse vs')))
 transformDeclaration (PatBind _ (PVar (Ident i)) (UnGuardedRhs b) _) = do
@@ -48,29 +48,59 @@ transformDeclaration (PatBind _ (PVar (Ident i)) (UnGuardedRhs b) _) = do
 transformDeclaration _ = Nothing
 
 transformExpression :: Exp -> Maybe Expression
+transformExpression (App p q) = do
+    p' <- transformExpression p
+    q' <- transformExpression q
+
+    return (p' :$ q')
 transformExpression (E.Case e ps) = do
     e' <- transformExpression e
 
     ps' <- (flip3 foldM) ps [] $ \a (Alt _ p b _) -> do
         b' <- case b of
             (UnGuardedRhs b') -> transformExpression b'
+        p' <- transformPattern p
 
-        p <- case p of
-            (PVar (Ident i)) -> return (Variable_p i)
-            (PApp (UnQual (Ident i)) as) -> do
-                as' <- (flip3 foldM) as [] $ \a p -> case p of
-                    (PVar (Ident i)) -> return (i:a)
-                    _ -> Nothing
-
-                return (Constructor i as')
-            PWildCard -> return WildCard
-
-        return ((p, b'):a)
+        return ((p', b'):a)
 
     return (Case e' ps')
+transformExpression (Con (UnQual (Ident i))) = return (Variable_e i)
+transformExpression (InfixApp p (QVarOp (UnQual (Symbol o))) q) = do
+    p' <- transformExpression p
+    q' <- transformExpression q
+    o' <- case o of
+        "+" -> return Add
+        "-" -> return Subtract
+        "*" -> return Multiply
+        "/" -> return Divide
+        _ -> Nothing
+
+    return (Expression o' p' q')
+transformExpression (E.Lambda _ vs b) = do
+    b' <- transformExpression b
+    (v':vs') <- (flip3 foldM) vs [] $ \a v -> case v of
+        (PVar (Ident i)) -> return (i:a)
+        _ -> Nothing
+
+    return (foldr (\v a -> Lambda v a) (Lambda v' b') (reverse vs'))
 transformExpression (Lit (Int i)) = return (Constant (fromIntegral i))
 transformExpression (Lit (String s)) = return (Str s)
+transformExpression (Paren e) = do
+    e' <- transformExpression e
+    return e'
 transformExpression (Var (UnQual (Ident i))) = return (Variable_e i)
 transformExpression _ = Nothing
+
+transformPattern :: Pat -> Maybe Pattern
+transformPattern (PVar (Ident i)) = return (Variable_p i)
+transformPattern (PApp (UnQual (Ident i)) as) = do
+    as' <- (flip3 foldM) as [] $ \a p -> case p of
+        (PVar (Ident i)) -> return (i:a)
+        _ -> Nothing
+
+    return (Constructor i as')
+transformPattern (PParen p) = transformPattern p
+transformPattern PWildCard = return WildCard
+transformPattern _ = Nothing
 
 flip3 f a b c = f c b a
